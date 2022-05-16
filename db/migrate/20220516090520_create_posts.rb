7991f305a6d1c1484093a026c89c8eaabad8bda3
Aratahead <maramhisa.1229@gmail.com>
2022-05-17 00:21:13 +0900

    postに関するcrud処理実装 問題名 コード　コメント投稿可能 #2

---
+++ db/migrate/20220516090520_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :question_name
+      t.text :code
+      t.text :comment
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
