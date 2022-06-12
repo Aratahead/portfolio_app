52f37fcd6c5dd80e950d1494160510a9a5d2358e
Aratahead <maramhisa.1229@gmail.com>
2022-06-12 15:00:54 +0900

    コード修正 #61

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,9 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: %i[update destroy]
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :ensure_normal_user, only: %i[update destroy]
 
-  def ensure_normal_user
-    redirect_to root_path, alert: "ゲストユーザーは削除できません" if resource.email == "guest@example.com"
+    def ensure_normal_user
+      redirect_to root_path, alert: "ゲストユーザーは削除できません" if resource.email == "guest@example.com"
+    end
   end
 end
