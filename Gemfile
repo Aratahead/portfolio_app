cd3a11cd82883882d93a52c39c069673eac1e597
Aratahead <maramhisa.1229@gmail.com>
2022-06-15 00:58:09 +0900

    正誤,総投稿数などのグラフ表示 #67

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem "mini_magick"
 gem "rails-i18n", "~> 6.0"
 gem "ransack"
 gem "chartkick"
+gem "groupdate"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,3 +1,10 @@
 class GraphsController < ApplicationController
-  def index; end
+  def index
+    @post = current_user.posts
+    @ac_num = @post.where(correct: "AC").count
+    @commentary_ac_num = @post.where(correct: "解説AC").count
+    @tle_num = @post.where(correct: "TLE").count
+    @wa_num = @post.where(correct: "WA").count
+    @correct_ratio = @post.count.zero? ? 0 : (100 * @ac_num / @post.count).to_f
+  end
 end
